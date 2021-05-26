322705c4b7934d9b7472d0aa0407cb5faca4cd62
Miho <zmyhail@gmail.com>
2021-05-26 19:02:34 +0300

    done tests

---
+++ tests/test_items.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_should_see_btn_add_to_basket(browser):
+    browser.get(link)
+    fidb = browser.find_element_by_css_selector(".btn-add-to-basket")
+    # print(type(fidb))
+    time.sleep(5)
+
+    assert isinstance(fidb, webdriver.remote.webelement.WebElement) == True, 'Site does not contain an add to backet'
